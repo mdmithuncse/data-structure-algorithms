bbaaaf8197d80d862f963e9e4dd1a601b4166da9
mdmithuncse <nuruzzaman@7peakssoftware.com>
2021-07-25 21:40:48 +0700

    add stack data structure

---
+++ DataStructure/StackDataStructure/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StackDataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to stack data structure example");
+
+            Stack stack = new Stack(10);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+            stack.Push(7);
+            stack.Push(8);
+            stack.Push(9);
+            stack.Push(10);
+
+            stack.PrintStack();
+
+            stack.Pop();
+
+            stack.PrintStack();
+
+            stack.Push(11);
+
+            stack.PrintStack();
+        }
+    }
+}
---
+++ DataStructure/StackDataStructure/Stack.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDataStructure
+{
+    public class Stack
+    {
+        private int[] array;
+        private int top;
+        private int capacity;
+
+        // Create a stack
+        public Stack(int size)
+        {
+            array = new int[size];
+            capacity = size;
+            top = -1;
+        }
+
+        // Add an item into stack
+        public void Push(int item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Stack is full.");
+            }
+
+            Console.WriteLine($"Adding item: {item}");
+            array[++top] = item;
+        }
+
+        // Remove an item from stack
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+            }
+
+            return array[top--];
+        }
+
+        // Check stack is full
+        public bool IsFull()
+        {
+            return top == capacity - 1;
+        }
+
+        // Check stack is empty
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        // Return the size of the stack
+        public int Size()
+        {
+            return top + 1;
+        }
+
+        // Print the elements of the stack
+        public void PrintStack()
+        {
+            for (int i = 0; i <= top; i++)
+            {
+                Console.WriteLine($"Item : {array[i]}");
+            }
+        }
+    }
+}
