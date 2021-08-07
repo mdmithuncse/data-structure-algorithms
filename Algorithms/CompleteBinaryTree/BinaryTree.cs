a9ece3e7bc4fbff48c503f59937ef3a008baf13a
mdmithuncse <nuruzzaman@7peakssoftware.com>
2021-08-07 22:47:41 +0700

    Check for complete binary tree

---
+++ Algorithms/CompleteBinaryTree/BinaryTree.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteBinaryTree
+{
+    public class BinaryTree
+    {
+        public Node root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        // Count total number of nodes
+        public int CountNumberOfNodes(Node node)
+        {
+            if (node == null)
+                return 0;
+
+            return 1 + CountNumberOfNodes(node.left) + CountNumberOfNodes(node.right);
+        }
+
+        // Check for complete binary tree
+        public bool IsCompleteBinaryTree(Node node, int index, int numberOfNodes)
+        {
+            if (node == null)
+                return true;
+
+            if (index >= numberOfNodes)
+                return false;
+
+            return IsCompleteBinaryTree(node.left, 2 * index + 1, numberOfNodes) && IsCompleteBinaryTree(node.right, 2 * index + 2, numberOfNodes);
+        }
+    }
+}
---
+++ Algorithms/CompleteBinaryTree/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CompleteBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to complete binary tree check example");
+
+            // Tree construction
+            BinaryTree tree = new BinaryTree();
+            tree.root = new Node(1);
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+            tree.root.left.right = new Node(5);
+            tree.root.right.left = new Node(6);
+
+            int nodeCount = tree.CountNumberOfNodes(tree.root);
+            int index = 0;
+
+            // Check for complete binary tree
+            if (tree.IsCompleteBinaryTree(tree.root, index, nodeCount))
+            {
+                Console.WriteLine("This is a complete binary tree");
+            }
+
+            else
+            {
+                Console.WriteLine("This isn't a complete binary tree");
+            }
+        }
+    }
+}
