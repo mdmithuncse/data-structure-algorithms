af1f673294105ce669e557904efeff3ab1c0c657
mdmithuncse <nuruzzaman@7peakssoftware.com>
2021-08-07 21:54:59 +0700

    Check perfect binary tree

---
+++ Algorithms/PerfectBinaryTree/BinaryTree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectBinaryTree
+{
+    public class BinaryTree
+    {
+        public Node root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        // Calculate depth
+        public static int Depth(Node node)
+        {
+            int d = 0;
+
+            while(node != null)
+            {
+                d++;
+                node = node.left;
+            }
+
+            return d;
+        }
+
+        // Check for the perfect binary tree
+        private bool IsPerfectBinaryTree(Node node, int depth, int level)
+        {
+            if (node == null)
+                return true;
+
+            if (node.left == null && node.right == null)
+                return (depth == level + 1);
+
+            return IsPerfectBinaryTree(node.left, depth, level + 1) && IsPerfectBinaryTree(node.right, depth, level + 1);
+        }
+
+        // Check for the perfect binary tree
+        public bool IsPerfectBinaryTree(Node node)
+        {
+            int depth = Depth(node);
+            return IsPerfectBinaryTree(node, depth, 0);
+        }
+    }
+}
---
+++ Algorithms/PerfectBinaryTree/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PerfectBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to perfect binary tree check example");
+
+            // Tree construction
+            BinaryTree tree = new BinaryTree();
+            tree.root = new Node(1);
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+            tree.root.left.right = new Node(5);
+            tree.root.right.left = new Node(6);
+            tree.root.right.right = new Node(7);
+
+            // Check for the perfect binary tree
+            if (tree.IsPerfectBinaryTree(tree.root))
+            {
+                Console.WriteLine("This is a perfect binary tree");
+            }
+
+            else
+            {
+                Console.WriteLine("This isn't a perfect binary tree");
+            }
+        }
+    }
+}
