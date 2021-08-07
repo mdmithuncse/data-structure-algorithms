6e71699630e31eb67c1445de0aeb8399b220c257
mdmithuncse <nuruzzaman@7peakssoftware.com>
2021-08-07 20:41:13 +0700

    check full binary tree

---
+++ Algorithms/FullBinaryTree/BinaryTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullBinaryTree
+{
+    public class BinaryTree
+    {
+        public Node root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        // Check for full binary tree
+        public bool IsFullBinaryTree(Node node)
+        {
+            if (node == null)
+                return true;
+
+            if (node.left == null && node.right == null)
+                return true;
+
+            if (node.left != null && node.right != null)
+                return IsFullBinaryTree(node.left) && IsFullBinaryTree(node.right);
+
+            return false;
+        }
+    }
+}
---
+++ Algorithms/FullBinaryTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FullBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to full binary tree check example");
+
+            // Tree construction
+            BinaryTree tree = new BinaryTree();
+            tree.root = new Node(1);
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+            tree.root.left.right = new Node(5);
+            tree.root.right.left = new Node(6);
+            tree.root.right.right = new Node(7);
+
+            // Check full binary tree
+            Console.WriteLine("Check full binary tree");
+            if (tree.IsFullBinaryTree(tree.root))
+                Console.WriteLine("The tree is a full binary tree");
+            else
+                Console.WriteLine("The tree isn't a full binary tree");
+        }
+    }
+}
