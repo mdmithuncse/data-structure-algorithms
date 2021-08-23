d6e5bac141bf9e080f2fc8ab9d56b09a9c578b87
mdmithuncse <nuruzzaman@7peakssoftware.com>
2021-08-23 15:40:07 +0700

    Largest element in array

---
+++ Research/LargestElementArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LargestElementArray
+{
+    class Program
+    {
+        // n = 5
+        // A[] = {1, 8, 7, 56, 90}
+
+        //n = 7
+        //A[] = {1, 2, 0, 3, 2, 4, 5}
+        static void Main(string[] args)
+        {
+            //int[] input = new int[] { 1, 8, 7, 56, 90 };
+            int[] input = new int[] { 1, 2, 0, 3, 2, 4, 5 };
+            int output = Largest(input, 7);
+            Console.WriteLine($"Largest element in array: { output }");
+        }
+
+        public static int Largest(int [] input, int n)
+        {
+            int max = input[0];
+
+            for(int i = 1; i < n; i++)
+            {
+                if (input[i] > max)
+                {
+                    max = input[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
