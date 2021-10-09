1448d23786d5ca15ffffcd4c39a9403aada61dca
mdmithun <nuruzzaman@7peakssoftware.com>
2021-10-09 11:56:31 +0700

    bubble sort

---
+++ Algorithms/BubbleSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] input = new int[5] { -2, 45, 0, 11, -9 };
+            Console.WriteLine("Bubble sorted array:");
+            foreach (var item in BubbleSort(input))
+                Console.Write($"{item} --> ");
+        }
+
+        private static int[] BubbleSort(int[] input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int j = 0; j < input.Length - i - 1; j++)
+                {
+                    if (input[j] > input[j + 1])
+                    {
+                        int temp = input[j];
+                        input[j] = input[j + 1];
+                        input[j + 1] = temp;
+                    }
+                }
+            }
+
+            return input;
+        }
+    }
+}
