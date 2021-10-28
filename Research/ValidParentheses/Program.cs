2a7adf1b6bceba14d6779b55f9d0e46e7159711e
mdmithun <nuruzzaman@7peakssoftware.com>
2021-10-28 22:59:03 +0700

    valid parentheses problem

---
+++ Research/ValidParentheses/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string input = "()[]{}";
+            Console.WriteLine($"Balanced Parentheses: {IsValid(input)}");
+        }
+
+        private static bool IsMatchPair(char ch1, char ch2)
+        {
+            if (ch1 == '(' && ch2 == ')')
+            {
+                return true;
+            }
+            else if (ch1 == '{' && ch2 == '}')
+            {
+                return true;
+            }
+            else if (ch1 == '[' && ch2 == ']')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsValid(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input)) return false;
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {                
+                if (input[i] == '(' ||
+                    input[i] == '{' ||
+                    input[i] == '[')
+                {
+                    stack.Push(input[i]);
+                }
+
+                else if (input[i] == ')' ||
+                         input[i] == '}' ||
+                         input[i] == ']')
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    else if (!IsMatchPair(stack.Pop(), input[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
