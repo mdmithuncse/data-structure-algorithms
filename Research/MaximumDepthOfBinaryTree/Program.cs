f43f72ab8a0653327dbea13c7451d529e6becdca
mdmithuncse <md.mithuncse@yahoo.com>
2022-02-06 13:26:34 +0700

    maximum depth of binary tree

---
+++ Research/MaximumDepthOfBinaryTree/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MaximumDepthOfBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int left = MaxDepth(root.left);
+            int right = MaxDepth(root.right);
+
+            return Math.Max(left, right) + 1;
+        }
+    }
+
+    /// <summary>
+    /// Definition for a binary tree node
+    /// </summary>
+    public class TreeNode
+    {
+        private int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+}
