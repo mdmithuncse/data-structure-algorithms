a828ceaa0b82df6a3aab4772aa0e1c955ca62935
mdmithuncse <md.mithuncse@yahoo.com>
2022-02-06 22:31:54 +0700

    minimum depth of binary tree

--- Research/MinimumDepthOfBinaryTree/Program.cs
+++ Research/MinimumDepthOfBinaryTree/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace MaximumDepthOfBinaryTree
+namespace MinimumDepthOfBinaryTree
 {
     class Program
     {
@@ -9,32 +9,37 @@ namespace MaximumDepthOfBinaryTree
             Console.WriteLine("Hello World!");
         }
 
-        static int MaxDepth(TreeNode root)
+        static int MinDepth(TreeNode root)
         {
             if (root == null)
             {
                 return 0;
             }
 
-            int left = MaxDepth(root.left);
-            int right = MaxDepth(root.right);
+            int left = MinDepth(root.left);
+            int right = MinDepth(root.right);
 
-            return Math.Max(left, right) + 1;
+            if (left == 0 || right == 0)
+            {
+                return left + right + 1;
+            }
+
+            return Math.Min(left, right) + 1;
         }
     }
 
-    /// <summary>
-    /// Definition for a binary tree node
-    /// </summary>
-    /// <param name="val"></param>
-    /// <param name="left"></param>
-    /// <param name="right"></param>
     public class TreeNode
     {
         public int val;
         public TreeNode left;
         public TreeNode right;
 
+        /// <summary>
+        /// Definition for a binary tree node
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
         public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
         {
             this.val = val;
