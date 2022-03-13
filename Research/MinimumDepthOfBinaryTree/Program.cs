471f39f7e6235697d19790f60f200c525fb8dd86
mdmithuncse <md.mithuncse@yahoo.com>
2022-03-13 11:06:04 +0700

    code simplify

--- Research/MinimumDepthOfBinaryTree/Program.cs
+++ Research/MinimumDepthOfBinaryTree/Program.cs
@@ -11,18 +11,12 @@ namespace MinimumDepthOfBinaryTree
 
         static int MinDepth(TreeNode root)
         {
-            if (root == null)
-            {
-                return 0;
-            }
+            if (root == null) return 0;
 
             int left = MinDepth(root.left);
             int right = MinDepth(root.right);
 
-            if (left == 0 || right == 0)
-            {
-                return left + right + 1;
-            }
+            if (left == 0 && right == 0) return 1;
 
             return Math.Min(left, right) + 1;
         }
