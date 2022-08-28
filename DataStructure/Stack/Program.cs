0d1ba55b954e16634f939290c031ab92725dae8b
mdmithuncse <md.mithuncse@yahoo.com>
2022-08-28 22:08:10 +0700

    restructure stack project

--- DataStructure/Stack/Program.cs
+++ DataStructure/Stack/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace StackDataStructure
+namespace Stack
 {
     class Program
     {
--- DataStructure/Stack/Stack.cs
+++ DataStructure/Stack/Stack.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StackDataStructure
+namespace Stack
 {
     public class Stack
     {
