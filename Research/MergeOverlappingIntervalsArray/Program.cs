edcc87e4f108a70c644494be586c61ad86b1b90d
mdmithuncse <md.mithuncse@yahoo.com>
2022-10-09 23:04:22 +0700

    merge overlapper array

--- Research/MergeOverlappingIntervalsArray/Program.cs
+++ Research/MergeOverlappingIntervalsArray/Program.cs
@@ -9,21 +9,33 @@ namespace MergeOverlappingIntervalsArray
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            // intervals = [[1,3],[2,6],[8,10],[15,18]]
-            // intervals = [[1,4],[4,5]]
-            int[][] input = new int[4][]
-            {
-                new int[] { 8, 10 },
-                new int[] { 15, 18 },
-                new int[] { 1, 3 },
-                new int[] { 2, 6 }
-            };
+
+            //int[][] input = new int[4][]
+            //{
+            //    new int[] { 8, 10 },
+            //    new int[] { 15, 18 },
+            //    new int[] { 1, 3 },
+            //    new int[] { 2, 6 }
+            //};
 
             //int[][] input = new int[2][]
             //{
             //    new int[] { 1, 4 },
             //    new int[] { 4, 5 }
             //};
+
+            //int[][] input = new int[2][]
+            //{
+            //    new int[] { 1, 4 },
+            //    new int[] { 5, 6 }
+            //};
+
+            //int[][] input = new int[1][]
+            //{
+            //    new int[] { 1, 4 }
+            //};
+
+            int[][] input = new int[][] { };
 
             int[][] result = Merge(input);
 
@@ -38,30 +50,21 @@ namespace MergeOverlappingIntervalsArray
 
         static int[][] Merge(int[][] intervals)
         {
-            if (intervals == null || intervals.Length == 0)
-                return new int[][] { };
-
             intervals = intervals.OrderBy(x => x[0]).ToArray();
-            int start = intervals[0][0];
-            int end = intervals[0][1];
-
             IList<int[]> merged = new List<int[]>();
 
-            for(int i = 1; i < intervals.Length; i++)
+            for (int i = 0; i < intervals.Length; i++)
             {
-                if (intervals[i][0] > end)
+                // if the list of merged intervals is empty or if the interval does not overlap with the previous, simply append it
+                if (!merged.Any() || merged.Last()[1] < intervals[i][0])
                 {
-                    start = intervals[i][0];
-                    end = intervals[i][1];
-
-                    merged.Add(new int[] { start, end });
+                    merged.Add(new int[] { intervals[i][0], intervals[i][1] });
                 }
 
+                // otherwise there is overlap, so we merge the current and previous intervals
                 else
                 {
-                    end = Math.Max(end, intervals[i][1]);
-
-                    merged.Add(new int[] { start, end });
+                    merged.Last()[1] = Math.Max(merged.Last()[1], intervals[i][1]);
                 }
             }
 
