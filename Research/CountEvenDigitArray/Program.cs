338ead30ea36b132535f7e8f2bcb9b0bb1595309
mdmithuncse <md.mithuncse@yahoo.com>
2022-10-16 20:17:40 +0700

    count even digit from array

---
+++ Research/CountEvenDigitArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CountEvenDigitArray
+{
+    class Program
+    {
+        // nums: [2, 10, 2562, 563, 789]
+        // output: 2
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] nums = new int[] { 2, 10, 2562, 563, 789 };
+            Console.WriteLine($"The output is: { CountEvenDigit(nums) }");
+        }
+
+        static int CountEvenDigit(int[] nums)
+        {
+            int count = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int length = 0;
+
+                while (nums[i] != 0)
+                {
+                    nums[i] = nums[i] / 10;
+                    length++;
+                }
+
+                if (length % 2 == 0)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
