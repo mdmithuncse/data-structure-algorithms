9b25db84928d6c299a2908b3fbf15e98306b2b2d
mdmithuncse <md.mithuncse@yahoo.com>
2022-10-18 23:11:51 +0700

    check if n and it's duplicate exists

--- Research/RemoveDuplicatesFromSortedArray/Program.cs
+++ Research/RemoveDuplicatesFromSortedArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RemoveDuplicatesFromSortedArray
 {
@@ -9,6 +10,26 @@ namespace RemoveDuplicatesFromSortedArray
             Console.WriteLine("Hello World!");
             int[] input = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
             Console.WriteLine($"Output is: {RemoveDuplicates(input)}");
+            //int[] nums = new int[] { 10, 2, 5, 3 };
+            int[] nums = new int[] { 7, 1, 14, 11 };
+            Console.WriteLine($"Output is: { CheckIfNAndItsDuplicateExists(nums) }");
+        }
+
+        static bool CheckIfNAndItsDuplicateExists(int[] nums)
+        {
+            HashSet<int> items = new HashSet<int>();
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(items.Contains(2 * nums[i]) || (nums[i] % 2 == 0 && items.Contains(nums[i] / 2)))
+                {
+                    return true;
+                }
+
+                items.Add(nums[i]);
+            }
+
+            return false;
         }
 
         static int RemoveDuplicates(int[] nums)
