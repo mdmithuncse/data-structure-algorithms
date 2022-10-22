98c48d3c660df65b6e9266282e916a50f00f0337
mdmithuncse <md.mithuncse@yahoo.com>
2022-10-23 00:23:54 +0700

    solve two array problem

---
+++ Research/MoveZerosToEnd/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MoveZerosToEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] nums = new int[] { 0, 1, 0, 3, 12 };
+
+            //Output: [1,3,12,0,0]
+
+            Console.WriteLine($"The output is: { MoveZeroes(nums) }");
+        }
+
+        static int[] MoveZeroes(int[] nums)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == 0)
+                {
+                    int j = i + 1;
+
+                    while(j < nums.Length)
+                    {
+                        if(nums[j] != 0)
+                        {
+                            int temp = nums[i];
+
+                            nums[i] = nums[j];
+                            nums[j] = temp;
+
+                            break;
+                        }
+
+                        j++;
+                    }
+                }
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ Research/ReplaceElementsWithGreatestElementOnRightSide/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReplaceElementsWithGreatestElementOnRightSide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] arr = new int[] { 17, 18, 5, 4, 6, 1};
+            Console.WriteLine($"The output is: { ReplaceElements(arr) }");
+        }
+
+        static int[] ReplaceElements(int[] arr)
+        {
+            int max = -1;
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                int temp = arr[i];
+                arr[i] = max;
+
+                if (temp > max)
+                    max = temp;
+            }
+
+            return arr;
+        }
+    }
+}
