7aec0f39afbe1d14adf04ba3bc7868e35317da94
mdmithuncse <md.mithuncse@yahoo.com>
2022-11-07 21:03:49 +0700

    number of good pairs

---
+++ Research/NumberOfGoodPairs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MaximumNumberOfWordsFoundInSentences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] sentences = new string[]
+            {
+                "alice and bob love leetcode",
+                "i think so too",
+                "this is great thanks very much"
+            };
+            Console.WriteLine($"The output is: { MostWordsFound(sentences) }");
+        }
+
+        public static int MostWordsFound(string[] sentences)
+        {
+            int max = 0;
+
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                max = Math.Max(max, sentences[i].Split(' ').Length);
+            }
+
+            return max;
+        }
+    }
+}
