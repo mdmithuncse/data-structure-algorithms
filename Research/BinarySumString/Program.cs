59b5eec31eb5a430d7362409030038b76a6d7b60
mdmithuncse <md.mithuncse@yahoo.com>
2022-11-16 22:32:48 +0700

    binary sum string

---
+++ Research/BinarySumString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BinarySumString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string a = "1010";
+            string b = "1011";
+            Console.WriteLine($"Binary sum is: { AddBinary(a, b) }");
+        }
+
+        static string AddBinary(string a, string b)
+        {
+            string result = string.Empty;
+            int s = 0;
+            int i = a.Length - 1;
+            int j = b.Length - 1;
+
+            while(i >= 0 || j >= 0 || s == 1)
+            {
+                s += i >= 0 ? a[i] - '0' : 0;
+                s += j >= 0 ? b[j] - '0' : 0;
+
+                result = (char)(s % 2 + '0') + result;
+
+                s /= 2;
+                i--; j--;
+            }
+
+            return result;
+        }
+    }
+}
