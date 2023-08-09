e08f53187e1335fd6502a5f03e5e760d97b254fb
mdmithuncse <mdmithuncse@gmail.com>
2023-08-09 21:14:33 +0700

    factorial number example

---
+++ Research/Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, this is factorial example");
+Console.WriteLine("Please input a number:");
+int inputNumber = int.Parse(Console.ReadLine());
+Console.WriteLine($"The factorial of {inputNumber} is {Factorial(inputNumber)}");
+
+int Factorial(int n)
+{
+    int i = 1;
+    int factorial = 1;
+
+    while (i <= n)
+    {
+        factorial *= i;
+        i++;
+    }
+
+    return factorial;
+}
